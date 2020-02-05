4b3ccfad119321c10f248bfe67184b56a0aec726
Alex Kim <alexkim612@gmail.com>
2020-02-04 22:12:33 -0800

    working proxy

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const app = express()
-const port = 1000
+const app = express();
+const port = 1000;
 const path = require('path');
 const morgan = require('morgan');
+
 
 //serve files from dist
 app.use(express.static(path.join(__dirname, '../client')));
@@ -13,8 +14,6 @@ app.use(morgan('dev'));
 //JSON requests
 app.use(express.json());
 
-// //route all requests
-// app.use('/', router);
 
 //notification on open server
 app.listen(port, () => console.log(`listening on port ${port}!`));
